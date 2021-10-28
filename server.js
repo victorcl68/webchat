337ba528a1de0ece7a01268f446cc8f46cb5538c
Victor Lopes <victor.cabrera0209@gmail.com>
2021-10-28 13:45:19 -0300

    Update PORT

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const express = require('express');
 
 const app = express();
 const http = require('http').createServer(app);
-const cors = require('cors');
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 const io = require('socket.io')(http, {
   cors: {
@@ -21,7 +20,6 @@ require('./sockets/chat')(io);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.use(cors());
 app.use(express.static(`${__dirname}/public`));
 
 app.get('/', chatController);
