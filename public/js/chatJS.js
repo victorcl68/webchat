4818fd1afd5e4822c75a2c15fb8094baf3f7ecd7
Victor Lopes <victor.cabrera0209@gmail.com>
2021-10-29 10:51:34 -0300

    refactor(new function): create a function that creates a random nickname

--- public/js/chatJS.js
+++ public/js/chatJS.js
@@ -6,3 +6,16 @@ const chatMessageValue = document.querySelector('.messageInput');
 const nicknameValue = document.querySelector('.nicknameInput');
 
 let nickname = '';
+
+const randomNickname = (length) => {
+  let result = '';
+
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i += 1) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
