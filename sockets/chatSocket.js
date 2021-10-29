eaeca0e0b20a73d9b97c0b7d7be0f8a21e8cd58e
Victor Lopes <victor.cabrera0209@gmail.com>
2021-10-29 15:29:49 -0300

    refactor: add a new 'socket.on' to insert the message on db

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,8 +1,13 @@
 const { nowDateFormatted } = require('../utils');
+const { insertMessage } = require('../models');
 
 module.exports = (io) => io.on('connection', (socket) => {
   socket.on('message', ({ nickname, chatMessage }) => {
     const message = `${nowDateFormatted()} - ${nickname}: ${chatMessage}`;
     io.emit('message', message);
+  });
+  socket.on('messageInfo', async ({ message, nickname, timestamp }) => {
+    const timestampNow = nowDateFormatted(timestamp);
+    await insertMessage(message, nickname, timestampNow);
   });
 });
