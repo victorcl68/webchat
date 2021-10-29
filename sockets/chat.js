44ed70685770e56eb34a3a880c9b9135f6f38ccf
Victor Lopes <victor.cabrera0209@gmail.com>
2021-10-29 10:27:10 -0300

    refactor(file name): change 'chat.js' to 'chatJS.js' and 'chat.js' to chatSocket.js'
    change 'chat.js' on path 'public/js' to 'chatJS.js' and 'chat.js' on 'sockets' folder to
    chatSocket.js'

--- sockets/chat.js
+++
@@ -1,5 +0,0 @@
-module.exports = (io) => io.on('connection', (socket) => {
-  socket.on('message', ({ nickname, chatMessage }) => {
-    io.emit('serverMessage', { nickname, chatMessage });
-  });
-});
