376fd79e489c916824d762ede7cd2e905796875a
Victor Lopes <victor.cabrera0209@gmail.com>
2021-11-03 10:11:26 -0300

    feat: add each nickname in users object and send it through socket.io

--- sockets/chatSocket.js
+++ sockets/chatSocket.js
@@ -1,10 +1,26 @@
 const { nowDateFormatted } = require('../utils');
 const { insertMessage } = require('../models');
 
+const users = {};
+// after many attempts I found this way to solve the problem of relating the socket id with the nickname 
+// of Marcos Leandro PR: https://github.com/tryber/sd-010-b-project-webchat/pull/61/
+
 module.exports = (io) => io.on('connection', (socket) => {
+  socket.on('connection', ({ nickname }) => {
+    users[socket.id] = nickname;
+    io.emit('users', users);
+  });
   socket.on('message', async ({ nickname, chatMessage }) => {
     const message = `${nowDateFormatted()} - ${nickname}: ${chatMessage}`;
     await insertMessage(message);
     io.emit('message', message);
+  });
+  socket.on('att', (nickname) => {
+    users[socket.id] = nickname;
+    io.emit('users', users);
+  });
+  socket.on('disconnect', () => {
+    delete users[socket.id];
+    io.emit('users', users);
   });
 });
