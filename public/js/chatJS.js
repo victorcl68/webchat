9cbf00425ba6304840244fac45e0faf6e3501289
Victor Lopes <victor.cabrera0209@gmail.com>
2021-11-03 10:12:09 -0300

    feat: add a function to create the list of users inside of DOM

--- public/js/chatJS.js
+++ public/js/chatJS.js
@@ -5,7 +5,26 @@ const nicknameSaveButton = document.querySelector('.nicknameSaveButton');
 const chatMessageValue = document.querySelector('.messageInput');
 const nicknameValue = document.querySelector('.nicknameInput');
 
+const dataTestid = 'data-testid';
 let nickname = '';
+
+const createUsers = (users) => {
+  const usersNicknames = Object.values(users);
+  const onlineUsers = document.querySelector('.onlineUsers');
+  const onlineUser = document.querySelector('.onlineUser').innerHTML;
+
+  onlineUsers.innerHTML = '';
+
+  usersNicknames.forEach((eachNickname) => {
+    if (onlineUser !== eachNickname) {
+      const newUser = document.createElement('li');
+      newUser.innerHTML = eachNickname;
+      newUser.setAttribute(dataTestid, 'online-user');
+      onlineUsers.appendChild(newUser);
+    }
+  });
+  return false;
+};
 
 const randomNickname = (length) => {
   let result = '';
@@ -24,9 +43,9 @@ const createNickname = () => {
   const h1 = document.createElement('h1');
   const titleDiv = document.querySelector('.titleDiv');
 
-  h1.setAttribute('data-testid', 'online-user');
   h1.className = 'onlineUser';
   h1.innerText = nickname;
+  h1.setAttribute(dataTestid, 'online-user');
 
   sessionStorage.setItem('nickname', nickname);
 
@@ -45,7 +64,7 @@ const createMessage = (message) => {
   const li = document.createElement('li');
 
   li.innerText = message;
-  li.setAttribute('data-testid', 'message');
+  li.setAttribute(dataTestid, 'message');
 
   messagesUl.appendChild(li);
   return false;
@@ -57,6 +76,7 @@ nicknameSaveButton.addEventListener('click', () => {
 
   deleteLastNickname();
   createNickname();
+  socket.emit('att', nickname);
   return false;
 });
 
@@ -76,8 +96,10 @@ sendButton.addEventListener('click', () => {
 });
 
 socket.on('message', (message) => createMessage(message));
+socket.on('users', (users) => createUsers(users));
 
 window.onload = () => {
   nickname = randomNickname(16);
   createNickname();
+  socket.emit('connection', { nickname: sessionStorage.getItem('nickname') });
 };
